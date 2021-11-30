cad181a2e47454609c348ccc8d54a19d3a8f625a
FireG45 <41199312+FireG45@users.noreply.github.com>
2021-11-30 17:43:31 +0300

    Добавьте файлы проекта.

---
+++ Gachinator/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace Gachinator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(40, 36);
+            this.InputTextBox.Multiline = true;
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.InputTextBox.Size = new System.Drawing.Size(291, 260);
+            this.InputTextBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(337, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 75);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(439, 329);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.InputTextBox);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ♂Gachinator ♂";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Gachinator/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gachinator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int min = 5;
+            int max = 9;
+            int k = 0;
+            string str = InputTextBox.Text;
+            string output = "";
+            string wrd = "";
+
+            Random rand = new Random();
+            string tstr = "";
+            for (int i = 0; i < str.Length; i++)
+                if (str[i] == ',' || str[i] == '.' || str[i] == '!' || str[i] == '"')
+                    tstr += ' ';
+                else
+                    tstr += str[i];
+
+
+            while (k < tstr.Length)
+            {
+                wrd = "";
+                for (int i = k; i < tstr.Length && tstr[i] != ' '; i++)
+                {
+                    wrd += tstr[i];
+                }
+                k += wrd.Length + 1;
+                int r = rand.Next(0, 10);
+
+                if (wrd == "фантазия")
+                    wrd = "♂deep dark fantasies♂";
+                else
+                if (wrd.Length > 3 && min <= r && r <= max)
+                {
+                    r = rand.Next(0, 10);
+
+                    int l = wrd.Length - 1;
+                    if (l > 0)
+                        switch (wrd[l])//окончания
+                        {
+                            case 'г':
+                                if (r > min && r < max)
+                                    wrd = "♂spanking♂";
+                                else
+                                    wrd = "♂gym♂";
+                                break;
+
+                            case 'е':
+                                if (r > min && r < max)
+                                    wrd = "♂slave♂";
+                                else
+                                    wrd = "♂wee wee♂";
+                                break;
+
+                            case 'м':
+                                wrd = "♂cum♂";
+                                break;
+
+                            case 'в':
+                                wrd = "♂latex glove♂";
+                                break;
+
+                            case 'н':
+                                if (r > min && r < max)
+                                    wrd = "♂semen♂";
+                                else
+                                    wrd = "♂lether man♂";
+                                break;
+
+                            case 'п':
+                                wrd = "♂fucking deep";
+                                break;
+
+                            case 'к':
+                                wrd = "♂dick♂";
+                                break;
+
+                            case 'й':
+                                if (r > min && r < max)
+                                    wrd = "♂fucking♂";
+                                else
+                                    wrd = "♂fisting♂";
+                                break;
+
+                            case 'р':
+                                if (r > min && r < max)
+                                    wrd = "♂boy next door♂";
+                                else
+                                    wrd = "♂dungeon master♂";
+                                break;
+
+                            case 'б':
+                                wrd = "♂boss of the gym♂";
+                                break;
+
+                            case 'ю':
+                                wrd = "♂fuck you♂";
+                                break;
+
+                            case 'и':
+                                if (r > min && r < max)
+                                    wrd = "♂jabroni♂";
+                                else
+                                    wrd = "♂Billy♂";
+                                break;
+
+                            case 'c':
+                                if (r > min && r < max)
+                                    wrd = "♂6 hot loads♂";
+                                else
+                                    wrd = "♂300$♂";
+                                break;
+
+                                //default:
+                                //    switch (wrd[0])//первая буква
+                                //    {
+                                //        case 'с':
+                                //            wrd = "♂semen♂";
+                                //            break;
+
+                                //        case 'а':
+                                //            wrd = "♂ass♂";
+                                //            break;
+
+                                //        case 'б':
+                                //            wrd = "♂boy next door♂";
+                                //            break;
+
+                                //        case 'ф':
+                                //            wrd = "♂fucking deep";
+                                //            break;
+
+                                //        case 'д':
+                                //            if (r > min && r < max)
+                                //                wrd = "♂dick♂";
+                                //            else
+                                //                wrd = "♂dungeon master♂";
+                                //            break;
+
+                                //        case 'т':
+                                //            wrd = "♂300$♂";
+                                //            break;
+                                //    }
+                                //    break;
+                        }
+
+
+                }
+
+                output += wrd + " ";
+
+            }
+
+
+
+
+            InputTextBox.Text = output;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
